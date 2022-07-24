308d5ccb21e56ec0637a6cd5b8240888507095c4
LHC <leehc0427@naver.com>
2022-07-25 08:42:21 +0900

    220725

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd"
+import styled from "styled-components";
+
+const Warpper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  max-width: 600px;
+  margin: 0 auto;
+`;
+const Board = styled.div`
+  padding: 20px 10px;
+  background-color: ${(props) => props.theme.borderColor};
+`;
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+  border-radius: 5px;
+`;
+const Card = styled.div`
+  padding: 10px;
+  margin: 5px 0;
+  border-radius: 5px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f",];
 
 function App() {
+  const onDragEnd = () => {}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Warpper>
+        <Boards>
+            <Droppable droppableId="one">{(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((toDo, index) => (<Draggable draggableId={toDo} index={index}>
+                  {(magic) => (
+                    <Card 
+                      ref={magic.innerRef} 
+                      {...magic.draggableProps}
+                      {...magic.dragHandleProps}
+                    >{toDo}</Card>
+                  )}
+                </Draggable>
+              ))}
+              {magic.placeholder}
+            </Board>)}
+          </Droppable>
+        </Boards>
+      </Warpper>
+    </DragDropContext>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components"
+
+export const theme: DefaultTheme = {
+    bgColor: "#3F8CF2",
+    borderColor: "#DADFE9",
+    cardColor: "white",
+}
+
+
